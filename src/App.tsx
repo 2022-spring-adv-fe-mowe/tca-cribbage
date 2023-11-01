facef813c6f0fd8bcde85553e50b98f0aa7752ed
ClaytonDewey <clay@dryadmedia.com>
2023-11-01 15:10:28 -0500

    reformat: Update React types

--- src/App.tsx
+++ src/App.tsx
@@ -1,141 +1,145 @@
-import "./App.scss";
-import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { useDarkMode } from "./Components/Theme/useDarkMode"
-import { GlobalStyles } from "./Components/Theme/GlobalStyles"
-import { lightTheme, darkTheme } from "./Components/Theme/Themes"
-import Toggle from "./Components/Theme/Toggler"
-import { Home } from "./Components/Home";
-import { PlayGame } from "./Components/PlayGame";
-import { SetupGame } from "./Components/SetupGame";
-import { Stats } from "./Components/Stats";
-import { Leaderboard } from "./Components/Leaderboard";
+import './App.scss';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './Components/Theme/useDarkMode';
+import { GlobalStyles } from './Components/Theme/GlobalStyles';
+import { lightTheme, darkTheme } from './Components/Theme/Themes';
+import Toggle from './Components/Theme/Toggler';
+import { Home } from './Components/Home';
+import { PlayGame } from './Components/PlayGame';
+import { SetupGame } from './Components/SetupGame';
+import { Stats } from './Components/Stats';
+import { Leaderboard } from './Components/Leaderboard';
 import { saveGameToCloud, loadGamesFromCloud } from './TcaCloudApi';
-import localforage from "localforage";
+import localforage from 'localforage';
 
 export interface Player {
-    name: string;
-    order: number;
+  name: string;
+  order: number;
 }
 
 export interface CurrentGame {
-    players: Player[];
-    start: string;
+  players: Player[];
+  start: string;
 }
 
 export interface GameResult {
-    start: string;
-    end: string;
-    winner: string;
-    players: Player[];
-    skunk?: boolean;
-    dblSkunk?: boolean;
-    skunked?: boolean;
-    dblSkunked?: boolean;
-    highPegg: number;
-    highHand: number;
-    highCrib: number;
+  start: string;
+  end: string;
+  winner: string;
+  players: Player[];
+  skunk?: boolean;
+  dblSkunk?: boolean;
+  skunked?: boolean;
+  dblSkunked?: boolean;
+  highPegg: number;
+  highHand: number;
+  highCrib: number;
 }
 
-export const User = "Me"
+export const User = 'Me';
 
-
-const getUniquePlayers = (results) => (
-    [...new Set(results.flatMap(x => x.players.map(y => y.name)))]
-);
+const getUniquePlayers = (results: GameResult[]) => [
+  ...new Set(results.flatMap((x) => x.players.map((y) => y.name))),
+];
 
 const App = () => {
+  const [results, setResults] = useState<GameResult[]>([]);
+  const [currentGame, setCurrentGame] = useState<CurrentGame>({
+    players: [],
+    start: '',
+  });
 
-    const [results, setResults] = useState<GameResult[]>([]);
-    const [currentGame, setCurrentGame] = useState<CurrentGame>({
-        players: []
-        , start: ""
-    });
+  const [emailAddress, setEmailAddress] = useState('');
 
-    const [emailAddress, setEmailAddress] = useState("");
+  const updateEmailAddress = async (newEmailAddress) => {
+    // Update the state, after saving the email in local storage.
+    setEmailAddress(await localforage.setItem('email', newEmailAddress));
+  };
 
-    const updateEmailAddress = async (newEmailAddress) => {
+  useEffect(() => {
+    const loadGameResults = async () => {
+      // Update the emailAddress state, after loading the email from local storage.
+      setEmailAddress((await localforage.getItem('email')) ?? '');
 
-        // Update the state, after saving the email in local storage.
-        setEmailAddress(await localforage.setItem('email', newEmailAddress));
-    }
-
-    useEffect(() => {
-        
-        const loadGameResults = async () => {
-
-            // Update the emailAddress state, after loading the email from local storage.
-            setEmailAddress(await localforage.getItem("email") ?? "");
-
-            // Then, if we have an email, load the game results.
-            if (emailAddress.length > 0) {
-                setResults(await loadGamesFromCloud(emailAddress, "tca-cribbage") ?? []);
-            }
-        };
-        
-        loadGameResults();
-
-    }, [emailAddress]);
-
-    const addGameResult = async (gameResult) => {
-
-        const newResults = [
-            ...results
-            , gameResult
-        ]
-
-        setResults(newResults);
-
-        // await localforage.setItem("gameResults", newResults);
-        await saveGameToCloud(
-            emailAddress
-            , 'tca-cribbage'
-            , gameResult.end   // new Date().toISOString()
-            , gameResult
-            );
+      // Then, if we have an email, load the game results.
+      if (emailAddress.length > 0) {
+        setResults(
+          (await loadGamesFromCloud(emailAddress, 'tca-cribbage')) ?? []
+        );
+      }
     };
 
-    const [theme, themeToggler] = useDarkMode();
-    const themeMode = theme === "light" ? lightTheme : darkTheme;
+    loadGameResults();
+  }, [emailAddress]);
 
-    return (
-        <ThemeProvider theme={themeMode}>
-            <GlobalStyles />
-            <div className="toggle__btn-container">
-                <Toggle theme={theme} toggleTheme={themeToggler} />
-            </div>
-            <main>
-                <Routes>
-                    <Route path="/" element={
-                        <Home
-                            emailAddress={emailAddress}
-                            updateEmailAddress={updateEmailAddress}
-                         />
-                    } />
-                    <Route path="setup" element={
-                        <SetupGame
-                            uniquePreviousPlayers={getUniquePlayers(results)}
-                            setCurrentGame={setCurrentGame}
-                        />
-                    } />
-                    <Route path="play" element={<PlayGame
-                            currentGame={currentGame}
-                            addGameResult={addGameResult}
-                     />} />
-                    <Route path="stats" element={
-                        <Stats
-                            gameResults={results}
-                        /> } />
-                    <Route path="leaderboard" element={
-                        <Leaderboard
-                            uniquePreviousPlayers={getUniquePlayers(results)}
-                            gameResults={results}
-                        /> } />
-                </Routes>
-            </main>
-        </ThemeProvider>
+  const addGameResult = async (gameResult: GameResult) => {
+    const newResults = [...results, gameResult];
+
+    setResults(newResults);
+
+    // await localforage.setItem("gameResults", newResults);
+    await saveGameToCloud(
+      emailAddress,
+      'tca-cribbage',
+      gameResult.end, // new Date().toISOString()
+      gameResult
     );
-}
+  };
+
+  const [theme, themeToggler] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <div className='toggle__btn-container'>
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+      </div>
+      <main>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Home
+                emailAddress={emailAddress}
+                updateEmailAddress={updateEmailAddress}
+              />
+            }
+          />
+          <Route
+            path='setup'
+            element={
+              <SetupGame
+                uniquePreviousPlayers={getUniquePlayers(results)}
+                setCurrentGame={setCurrentGame}
+              />
+            }
+          />
+          <Route
+            path='play'
+            element={
+              <PlayGame
+                currentGame={currentGame}
+                addGameResult={addGameResult}
+              />
+            }
+          />
+          <Route path='stats' element={<Stats gameResults={results} />} />
+          <Route
+            path='leaderboard'
+            element={
+              <Leaderboard
+                uniquePreviousPlayers={getUniquePlayers(results)}
+                gameResults={results}
+              />
+            }
+          />
+        </Routes>
+      </main>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,59 +1,64 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { cardsIcon } from "./Icons";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { cardsIcon } from './Icons';
 
 export const Home = ({ emailAddress, updateEmailAddress }) => {
-    const nav = useNavigate();
-    const [emailForEditing, setEmailForEditing] = useState(emailAddress);
+  const nav = useNavigate();
+  const [emailForEditing, setEmailForEditing] = useState(emailAddress);
 
-    return (
-        <>
-                <div className="container container-home">
-                    <h1 className="text-center my-2">Cribbage {cardsIcon()}</h1>
+  return (
+    <>
+      <div className='container container-home'>
+        <h1 className='text-center my-2'>Cribbage {cardsIcon()}</h1>
 
-                    { emailAddress.length > 0 ?
-                        <>
-                            <h3>{emailAddress}</h3>
-                            <button className="btn btn-success mt-2" onClick={() => nav("/setup")}>
-                                New Game <i className="fa-solid fa-circle-play"></i>
-                            </button>
+        {emailAddress.length > 0 ? (
+          <>
+            <h3>{emailAddress}</h3>
+            <button
+              className='btn btn-success mt-2'
+              onClick={() => nav('/setup')}>
+              New Game <i className='fa-solid fa-circle-play'></i>
+            </button>
 
-                            <button className="btn btn-info mt-2" onClick={() => nav("/stats")}>
-                                Stats <i className="fa-solid fa-chart-line"></i>
-                            </button>
-                            
-                            <button className="btn btn-warning mt-2" onClick={() => nav("/leaderboard")}>
-                                Leaderboard <i className="fa-solid fa-medal"></i>
-                            </button>
+            <button className='btn btn-info mt-2' onClick={() => nav('/stats')}>
+              Stats <i className='fa-solid fa-chart-line'></i>
+            </button>
 
-                            <button
-                                className="btn btn-danger mt-2"
-                                onClick={() => updateEmailAddress("")}
-                            >
-                                Sign Out <i className="fa-solid fa-right-from-bracket"></i>
-                            </button>
-                        </>
-                        :
-                        <>
-                            <div className="form-control">
-                                <input
-                                    required
-                                    value={emailForEditing}
-                                    onChange={(e) => setEmailForEditing(e.target.value)}
-                                />
-                                <label>
-                                    <span>Email Address</span>
-                                </label>
-                            </div>
-                            <button
-                                className="btn btn-success"
-                                onClick={() => updateEmailAddress(emailForEditing)}
-                            >
-                                Sign In <i className="fa-solid fa-right-to-bracket"></i>
-                            </button>
-                        </>
-                    }
-                </div>
-        </>
-    );
+            <button
+              className='btn btn-warning mt-2'
+              onClick={() => nav('/leaderboard')}>
+              Leaderboard <i className='fa-solid fa-medal'></i>
+            </button>
+
+            <button
+              className='btn btn-danger mt-2'
+              onClick={() => updateEmailAddress('')}>
+              Sign Out <i className='fa-solid fa-right-from-bracket'></i>
+            </button>
+          </>
+        ) : (
+          <>
+            <div className='form-control'>
+              <input
+                required
+                value={emailForEditing}
+                onChange={(e) =>
+                  setEmailForEditing(e.target.value.toLowerCase())
+                }
+                type='email'
+              />
+              <label>
+                <span>Email Address</span>
+              </label>
+            </div>
+            <button
+              className='btn btn-success'
+              onClick={() => updateEmailAddress(emailForEditing)}>
+              Sign In <i className='fa-solid fa-right-to-bracket'></i>
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
